a20b7d158c67a43ddc70027170c79e0b2bee5fcd
unknown <max>
2023-04-20 17:27:35 -0300

    comentario

---
+++ commands.sh
@@ -0,0 +1,29 @@
+local
+------------
+#ver estado de archvicos neuvos o q cambe
+git status 
+
+#agregar neuvos archivos para subrlos
+git add .
+
+#git commit, para subir los archivos le ponemos un comentario
+git commit -m "comentario"
+
+-----------------------
+nube
+#subir archivo a git cloud
+git push origin master
+
+master= nombre de branch
+
+#traer archuvos de cloud a mi local
+git pull origin master
+
+crear branch
+# git checkout -b nombre_branch
+
+#cambiar de branch
+git checkout nombre_branch
+
+
+
